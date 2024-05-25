25cfba9c94987b401e7f83c3cabe04232809116e
KLubina <kristian.lubina@outlook.com>
2024-05-25 18:35:43 +0200

    Squashed commit of the following:
    commit a42f96e88fd492866f58704b9d0651ff4f70c625
    Author: KLubina <kristian.lubina@outlook.com>
    Date:   Sat May 25 18:34:17 2024 +0200
    
        Update style.css
    
    commit af2e554797ac802088c03ec44d3d83a9efd9e64b
    Author: KLubina <kristian.lubina@outlook.com>
    Date:   Sat May 25 18:30:42 2024 +0200
    
        Update index.php
    
    commit c6f8e22a63581e0293af12ffe8b2ae428ca5f9c3
    Author: KLubina <kristian.lubina@outlook.com>
    Date:   Sat May 25 18:18:46 2024 +0200
    
        changes
    
    commit 6dc6a9cabc669965af6110de3a86b4ae562d57c7
    Author: KLubina <kristian.lubina@outlook.com>
    Date:   Fri May 24 18:52:32 2024 +0200
    
        Update README.md
    
    commit 8768043c6398a111dad4545598fcfbc8998c8246
    Author: KLubina <kristian.lubina@outlook.com>
    Date:   Fri May 24 18:49:54 2024 +0200
    
        changes
    
    commit 4bce0a46de2db0f26569f0c9c8a6d4d25cf4b447
    Author: Kristian Lubina <kristian.lubina@outlook.com>
    Date:   Sun May 19 14:45:01 2024 +0200
    
        Update README.md
    
    commit 6d8bb2b053db5fdcbb5781d058304fdf9683fa01
    Author: Kristian Lubina <kristian.lubina@outlook.com>
    Date:   Sun May 19 14:44:42 2024 +0200
    
        Create README.md

--- lernziele-modul-322-branch/README.md
+++ lernziele-modul-322-branch/README.md
@@ -2,4 +2,4 @@ https://raw.githack.com/
 
 https://rawcdn.githack.com/KLubina/My-Learning-Websites/6d8bb2b053db5fdcbb5781d058304fdf9683fa01/lernziele-modul-322-branch/index.html
 
-http://localhost/2.ZP/
+http://localhost/lern-webseite-zur-2ten-ZP/
---
+++ lernziele-modul-322-branch/lern-webseite-zur-2ten-ZP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2. ZP Lernziele</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Lernspiel 2. ZP Modul 322</h1>
+        <?php include 'spiel.php'; ?>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
