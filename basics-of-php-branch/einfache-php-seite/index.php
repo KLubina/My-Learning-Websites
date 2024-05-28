a2214e0e30e31092451e4b397093a4897fbb31ac
KLubina <kristian.lubina@outlook.com>
2024-05-28 21:41:21 +0200

    Revert "Merge branch 'basics-of-flow-charts'"
    This reverts commit b58a60321ff88311a3e7a29dd04ddbb031eb711d, reversing
    changes made to 798d79d6a1967d138f6174dd5f691ca8e892f5ed.

---
+++ basics-of-php-branch/einfache-php-seite/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Meine Webseite</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Willkommen auf meiner Webseite!</h1>
+    <p>Dies ist eine einfache PHP-Seite.</p>
+    <a href="about.php">Über uns</a>
+    <a href="contact.php">Kontakt</a>
+</body>
+</html>
